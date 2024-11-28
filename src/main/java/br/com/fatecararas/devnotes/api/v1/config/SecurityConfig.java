fca95cc67b103def5d8b2f358f592db4eae5ba62
marcelo.miranda2 <marcelofmiranda23@gmail.com>
2024-11-27 21:05:19 -0300

    testando novas classes

--- src/main/java/br/com/fatecararas/devnotes/api/v1/config/SecurityConfig.java
+++ src/main/java/br/com/fatecararas/devnotes/api/v1/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package br.com.fatecararas.devnotes.api.v1.config;public class SecurityConfig {
+package br.com.fatecararas.devnotes.api.v1.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
+
+@Configuration
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/public/**").permitAll()  // Allow access to public endpoints
+                .anyRequest().authenticated()          // Require authentication for other endpoints
+                .and()
+                .formLogin()  // Enable form-based login
+                .permitAll();
+    }
 }
--- src/main/java/br/com/fatecararas/devnotes/api/v1/config/WebSecurityConfig.java
+++ src/main/java/br/com/fatecararas/devnotes/api/v1/config/WebSecurityConfig.java
@@ -1,2 +1,34 @@
-package br.com.fatecararas.devnotes.api.v1.config;public class WebSecurityConfig {
+package br.com.fatecararas.devnotes.api.v1.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Recursos que não precisarão de autenticação
+    final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/api/v1/categorias" };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()  // Liberar recursos listados
+                .anyRequest().authenticated()            // Requer autenticação para outros recursos
+                .and()
+                .httpBasic()                              // Utilizar autenticação básica
+                .and()
+                .csrf()
+                .disable()                               // Desabilitar CSRF
+                .and()
+                .headers()
+                .frameOptions().disable();               // Habilitar o uso de frames para H2
+    }
 }
